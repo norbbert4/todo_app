ce17debf760a5cc669158d75ae4f7347c411f2c1
Andris723 <72316694573@szily.hu>
2025-02-27 14:14:07 +0100

    sql update

--- sql/todo_app.sql
+++ sql/todo_app.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Gép: 127.0.0.1
--- Létrehozás ideje: 2025. Feb 26. 22:54
--- Kiszolgáló verziója: 10.4.32-MariaDB
--- PHP verzió: 8.2.12
+-- Host: localhost
+-- Generation Time: Feb 19, 2025 at 09:17 PM
+-- Server version: 10.11.10-MariaDB
+-- PHP Version: 8.2.20
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,50 +18,132 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Adatbázis: `todo_app`
+-- Database: `todo_app`
 --
 
 -- --------------------------------------------------------
 
 --
--- Tábla szerkezet ehhez a táblához `todos`
+-- Table structure for table `logins`
+--
+
+CREATE TABLE `logins` (
+  `login_id` int(11) NOT NULL,
+  `login_user_id` int(11) DEFAULT NULL,
+  `login_date` timestamp NULL DEFAULT NULL,
+  `action_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `logout_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `login_token` varchar(32) DEFAULT NULL,
+  `login_state` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `logins`
+--
+
+INSERT INTO `logins` (`login_id`, `login_user_id`, `login_date`, `action_date`, `logout_date`, `login_token`, `login_state`) VALUES
+(1, 1, '2025-02-19 21:15:38', '2025-02-19 21:16:08', '2025-02-19 21:16:10', '3f77ebe04ced9da128e4d2567858a414', 0),
+(2, 2, '2025-02-19 21:16:31', '2025-02-19 21:16:54', '2025-02-19 21:16:57', '90089b2116e784467a95cb2199f0601c', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `todos`
 --
 
 CREATE TABLE `todos` (
   `id` int(11) NOT NULL,
   `user_id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `date` date NOT NULL,
   `completed` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- A tábla adatainak kiíratása `todos`
+-- Dumping data for table `todos`
 --
 
-INSERT INTO `todos` (`id`, `user_id`, `title`, `date`, `completed`) VALUES
-(1, 1, 'Ez egy feladat user számára', '0000-00-00', 0),
-(2, 1, 'Ez egy másik feladat user számára', '0000-00-00', 0);
+INSERT INTO `todos` (`id`, `user_id`, `title`, `completed`) VALUES
+(1, 1, 'Ez egy feladat user számára', 0),
+(2, 1, 'Ez egy másik feladat user számára', 0),
+(3, 2, 'Ez egy feladat admin számára', 0),
+(4, 2, 'Ez egy másik feladat admin számára', 1);
+
+-- --------------------------------------------------------
 
 --
--- Indexek a kiírt táblákhoz
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `user_ID` int(11) NOT NULL,
+  `user_email` varchar(255) NOT NULL,
+  `user_name` varchar(255) NOT NULL,
+  `user_pw` varchar(255) NOT NULL,
+  `user_state` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`user_ID`, `user_email`, `user_name`, `user_pw`, `user_state`) VALUES
+(1, 'user@domain.hu', 'user', '$2y$10$rx6C1ib2CDx/SqnpZW9BQ.kTPzOFM.l97nDUh1y8V7lQtu.a1cRuK', 1),
+(2, 'admin@domain.hu', 'admin', '$2y$10$kjrUKUPP/nbYWkSB7oJ27O6Y9cEaAPuDQ9h9R1ydULVFBFLWL6mWu', 1);
+
+--
+-- Indexes for dumped tables
 --
 
 --
--- A tábla indexei `todos`
+-- Indexes for table `logins`
+--
+ALTER TABLE `logins`
+  ADD PRIMARY KEY (`login_id`),
+  ADD KEY `login_user_id` (`login_user_id`);
+
+--
+-- Indexes for table `todos`
 --
 ALTER TABLE `todos`
   ADD PRIMARY KEY (`id`);
 
 --
--- A kiírt táblák AUTO_INCREMENT értéke
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`user_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT a táblához `todos`
+-- AUTO_INCREMENT for table `logins`
+--
+ALTER TABLE `logins`
+  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `todos`
 --
 ALTER TABLE `todos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `user_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `logins`
+--
+ALTER TABLE `logins`
+  ADD CONSTRAINT `logins_ibfk_1` FOREIGN KEY (`login_user_id`) REFERENCES `users` (`user_ID`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
