6557527d12c1d49a50c95a855c16062ebaf10169
norbbert4 <hargitainorbertviktor4@gmail.com>
2025-03-15 14:50:04 +0100

    webes logout.php javítva

--- api/authentication/logout.php
+++ api/authentication/logout.php
@@ -1,8 +1,8 @@
 <?php
-include '../modules/_db.php';
+include '../modules/_db.php'; // Helyi fájlra javítva
 
-$sql="UPDATE logins SET logout_date=NOW(), login_state=0 WHERE login_user_id=".$_GET['userid']." and login_state=1";
+$sql = "UPDATE logins SET logout_date=NOW(), login_state=0 WHERE login_user_id=" . $_GET['userid'] . " and login_state=1";
 $result = $conn->query($sql);
 $conn->close();
-header("location:../../");
+header("Location: https://todoapp.norbbert4.hu/index.html"); // Abszolút URL
 ?>
--- assets/js/logout_.js
+++ assets/js/logout_.js
@@ -1,3 +1,3 @@
-const userData = JSON.parse(localStorage.getItem('userData'));
+const userData = JSON.parse(localStorage.getItem('userData')) || { user_ID: '' };
 localStorage.clear();
-window.location.href = "https://todoapp.norbbert4.hu/api/authentication/logout.php?userid="+userData.user_ID;
+window.location.href = "https://todoapp.norbbert4.hu/api/authentication/logout.php?userid=" + (userData.user_ID || '');
