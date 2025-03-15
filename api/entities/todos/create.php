72c8409c489388998fd73653246e970a43c7a2ab
norbbert4 <hargitainorbertviktor4@gmail.com>
2025-03-15 14:04:02 +0100

    adatbázis kapcsolat javítása, create.php insert into javítása, sql javítása
    idáig nem működött a jegyzethozzáadás normálisan.

--- api/entities/todos/create.php
+++ api/entities/todos/create.php
@@ -1,6 +1,7 @@
 <?php
-$title = (string)$data['title'];
-$date = (string)$data['date'];
+$title = $conn->real_escape_string((string)$data['title']);
+$date = $conn->real_escape_string((string)$data['date']);
+$userID = (int)$userID;
 
-$sql = "INSERT todos SET user_id=$userID, title='$title', date='$date'";
+$sql = "INSERT INTO todos SET user_id=$userID, title='$title', date='$date', completed=0";
 ?>
